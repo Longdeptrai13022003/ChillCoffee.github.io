9ef47d7343d49a46a4609d8cdee7dff7213d342c
Trần Đức Long <tranduclong1302@gmail.com>
2024-11-08 00:19:11 +0700

    menu commit

--- coffeeData.js
+++ coffeeData.js
@@ -2,45 +2,45 @@ const coffeeData = [
   {
     src: "./assets/menu/matcha latte.avif",
     title: 'Matcha Latte',
-    description: 'Fragrant black coffee with Jameson Irish whiskey and whipped milk',
+    description: 'Matcha latte – thanh mát vị trà, dịu dàng vị sữa',
     cost: "35,000đ"
   },
   {
     src:"./assets/menu/coffee-3.png",
-    title: "Honey raf",
-    description: "Espresso with frothed milk, cream and aromatic honey",
-    cost: "$5.50"
+    title: "Espresso",
+    description: "Espresso – đậm đà cà phê, quyện vị sữa, thơm ngọt hương mật",
+    cost: "50,000đ"
   },
   {
-    src: "./assets/menu/coffee-4.png",
-    title: "Ice cappuccino",
-    description: "Cappuccino with soft thick foam in summer version with ice",
-    cost: "$5.00"
+    src: "./assets/menu/mocha.jpg",
+    title: "Mochaccino",
+    description: "Mochaccino – đắng nhẹ cà phê, ngọt ngào sô cô la, quyện vị sữa mịn.",
+    cost: "55,000đ"
   },
 
   {
-    src: "./assets/menu/coffee-5.png",
-    title: "Ice cappuccino",
-    description: "Cappuccino with soft thick foam in summer version with ice",
-    cost: "$5.00"
+    src: "./assets/menu/americano1.webp",
+    title: "Americano",
+    description: "Americano – đậm đà, thuần vị, chất cà phê nguyên bản.",
+    cost: "50,000đ"
   },
   {
-    src: "./assets/menu/coffee-6.png",
-    title: "Latte",
-    description: "Espresso coffee with the addition of steamed milk and dense milk foam",
-    cost: "$5.50"
+    src: "./assets/menu/bacxiu.jpg",
+    title: "Bạc xỉu",
+    description: "Thức uống cho kẻ nặng tình T_T",
+    cost: "30,000đ"
   },
   {
     src: "./assets/menu/coffee-7.png",
     title: "Latte macchiato",
-    description: "Espresso with frothed milk and chocolate",
-    cost: "$5.50"
+    description: "Latte Macchiato – tầng sữa béo mịn, điểm nhẹ hương cà phê.",
+    cost: "45,000đ"
   },
   {
-    src: "./assets/menu/coffee-8.png",
-    title: "Coffee with cognac",
-    description: "Fragrant black coffee with cognac and whipped cream",
-    cost: "$6.50"
+    src: "./assets/menu/caphecotduaa.jpg",
+    title: "Cà phê cốt dừa",
+    description: "Đậm đà người Hải Phòng",
+    cost: "35,000đ"
   },
 ]
 
--- teaData.js
+++ teaData.js
@@ -1,28 +1,28 @@
 const teaData = [
   {
-    src: "./assets/menu/tea/tea-1.png",
-    title: 'Moroccan',
-    description: 'Fragrant black coffee with Jameson Irish whiskey and whipped milk',
-    cost: "$4.50"
+    src: "./assets/menu/tea/oihong.jpg",
+    title: 'Trà ổi hồng',
+    description: 'Trà ổi hồng – tươi mới hương ổi, dịu dàng hòa quyện vị trà thanh',
+    cost: "35,000đ"
   },
   {
-    src: "./assets/menu/tea/tea-2.png",
-    title: "Ginger",
-    description: "Original black tea with fresh ginger, lemon and honey",
-    cost: "$5.00"
+    src: "./assets/menu/tea/tradao.webp",
+    title: "Trà đào cam sả",
+    description: "Trà đào cam sả – đào ngọt ngào, cam tươi mát, sả thanh thoát.",
+    cost: "35,000đ"
   },
   {
-    src: "./assets/menu/tea/tea-3.png",
-    title: "Cranberry",
-    description: "Invigorating black tea with cranberry and honey",
-    cost: "$5.00"
+    src: "./assets/menu/tea/traxoai.webp",
+    title: "Trà xoài chanh leo",
+    description: "Trà xoài chanh leo – hương vị nhiệt đới, thanh mát, chạm vào cảm xúc.",
+    cost: "35,000đ"
   },
 
   {
-    src: "./assets/menu/tea/tea-4.png",
-    title: "Sea buckthorn",
-    description: "Toning sweet black tea with sea buckthorn, fresh thyme and cinnamon",
-    cost: "$5.50"
+    src: "./assets/menu/tea/travai.webp",
+    title: "Trà vải",
+    description: "Trà vải – thơm ngon, mát lạnh, đậm đà hương vị.",
+    cost: "35,000đ"
   },
 ]
 
